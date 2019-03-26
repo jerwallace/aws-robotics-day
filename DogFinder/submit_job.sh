4b19dacefa30fa0442723c060e9791276d82df26
Allan MacInnis <ajmac@amazon.com>
2019-03-26 12:53:08 -0500

    Update submit_job.sh

--- DogFinder/submit_job.sh
+++ DogFinder/submit_job.sh
@@ -21,13 +21,7 @@ aws robomaker create-simulation-application \
 robot_app=`aws robomaker list-robot-applications | grep -Po '"\K(arn:.*DogFinder.*)"' | sed 's/.$//'`
 sim_app=`aws robomaker list-simulation-applications | grep -Po '"\K(arn:.*DogFinder.*)"' | sed 's/.$//'`
 
-aws robomaker create-simulation-job \
-  --max-job-duration-in-seconds=7200 \
-  --iam-role "$ROLE_ARN" \
-  --vpc-config "subnets=$SUBNETS,securityGroups=$SECURITY_GROUP,assignPublicIp=true" \
-  --output-location "s3Bucket=$BUCKET_NAME,s3Prefix=dogfinderOutput" \
-  --failure-behavior Fail \
-  --robot-applications \
-    "application=$robot_app,applicationVersion=\$LATEST,launchConfig={packageName=dogfinder_robot,launchFile=find_dog.launch}" \
-  --simulation-applications \
-    "application=$sim_app,applicationVersion=\$LATEST,launchConfig={packageName=dogfinder_simulation,launchFile=hexagon.launch}" 
+template='{"iamRole":"%s","outputLocation":{"s3Bucket":"%s","s3Prefix":"dogfinderOutput"},"maxJobDurationInSeconds":28800,"failureBehavior":"Fail","robotApplications":[{"application":"%s","applicationVersion":"$LATEST","launchConfig":{"packageName":"dogfinder_robot","launchFile":"find_dog.launch","environmentVariables":{}}}],"simulationApplications":[{"application":"%s","applicationVersion":"$LATEST","launchConfig":{"packageName":"dogfinder_simulation","launchFile":"hexagon.launch","environmentVariables":{}}}],"vpcConfig":{"subnets":["%s","%s"],"securityGroups":["%s"],"assignPublicIp":true}}'
+json_params=$(printf "$template" "$ROLE_ARN" "$BUCKET_NAME" "$robot_app" "$sim_app" "$SUBNET1" "$SUBNET2" "$SECURITY_GROUP")
+
+aws robomaker create-simulation-job --cli-input-json "$json_params"
